f7f6d10acbc2452de5785ad2298c7e11ce1e8b3e
ArtemFrun <49340615+ArtemFrun@users.noreply.github.com>
2019-04-20 21:37:58 +0300

    Update Trip.py

--- Trip.py
+++ Trip.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot(customData.TOKEN)
 conn = sqlite3.connect('mydb.sqlite')
 cursor = conn.cursor()
 
-
+###Создание поездки
 def trip_pas(dispatch, expectation, price, lon_pas, lat_pas, chat_id):
     global status
     status = 1
@@ -18,13 +18,22 @@ def trip_pas(dispatch, expectation, price, lon_pas, lat_pas, chat_id):
     con.commit()
     return
 
-
-def cancel_trip_pas(_status):
+###Отмена поездки пасажиром
+def cancel_trip_pas(_chat_id):
     with sqlite3.connect("mydb.sqlite") as con:
         cur = con.cursor()
-    cur.execute('UPDATE trip SET status="4"')
+    cur.execute('UPDATE trip SET status="4" WHERE chat_id=_chat_id AND status="1"')
     con.commit()
     return
+
+
+###Отображение активных заказов
+def active_search_passenger(_chat_id):
+    with sqlite3.connect("mydb.sqlite") as con:
+        cur = con.cursor()
+    active_trip = 'SELECT * FROM trip WHERE chat_id!=_chat_id AND status="1"'
+    cur.execute(active_trip)
+    active_search =
 
 
 
