e01d9d7c7dd6032cfa3046b6a02a2fb2c1253c93
ArtemFrun <49340615+ArtemFrun@users.noreply.github.com>
2019-04-29 12:54:37 +0300

    Update taxi_bot.py

--- taxi_bot.py
+++ taxi_bot.py
@@ -7,6 +7,8 @@ import customData
 import Trip
 import time
 from datetime import timedelta, datetime
+from geopy.distance import geodesic
+
 
 conn = sqlite3.connect('mydb.sqlite')
 cursor = conn.cursor()
@@ -65,6 +67,9 @@ def handle_text(call):
     elif call.data == "NO_pas_dr":
         bot.send_message(call.message.chat.id, "Номер телефона")
         bot.register_next_step_handler(call.message, get_phone_dr)
+    elif call.data == 'order':
+        bot.send_message(call.message.chat.id, "Хотите принять этот заказ?")
+        bot.register_next_step_handler(call.message, location_drive)
 
 
 
@@ -237,7 +242,7 @@ def location_pas(message):
         global lat_dr
         lon_dr = message.location.longitude
         lat_dr = message.location.latitude
-        bot.register_next_step_handler(message, location_drive(message))
+        bot.register_next_step_handler(message, location_drive(message, 0))
 
 
 
@@ -259,8 +264,7 @@ def expectation(message):
 def trip_price(message):
     global price
     global chat_id
-    time_Create_dr = time.strftime("%Y.%m.%d", time.localtime())
-    date_Create = time_Create_dr
+    date_Create = time.strftime("%Y.%m.%d", time.localtime())
     chat_id = message.chat.id
     price = int(message.text)
     bot.send_message(message.chat.id, "Поиск машины")
@@ -296,9 +300,55 @@ def conf_cancel_trip(message):
 ### Начало поиска пассажира
 
 
-def location_drive(message):
+def location_drive(message, i):
      bot.send_message(message.chat.id, "Начало поиска")
-     Trip.active_search_passenger(message, lon_dr, lat_dr)
+     now = datetime.now()
+     day = datetime(now.year, now.month, now.day)
+     rez = (now - day)
+     now_sec = int(rez.total_seconds())
+     data_Create_dr = time.strftime("%Y.%m.%d", time.localtime())
+     time_Create_dr = now_sec - 600
+     con = sqlite3.connect("mydb.sqlite")
+     cur = con.cursor()
+     with con:
+         cur.execute('SELECT * FROM trip WHERE data_seconds<? AND date_Create=? AND status_trip=1',
+                     (time_Create_dr, data_Create_dr))
+         while True:
+             row = cur.fetchone()
+
+             if row == None:
+                 markup_menu = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)  ###переход к поиску поездки
+                 btn_search = types.KeyboardButton('Поиск пассажира')
+                 markup_menu.add(btn_search)
+                 bot.send_message(message.chat.id, "Больше нету заказов. Для начала поиска нажмите \"Поиск пассажира\"",
+                                  reply_markup=markup_menu)
+                 i = 0
+                 break
+
+             result = float('{:.2f}'.format(geodesic((row[3], row[4]), (lon_dr, lat_dr)).km))
+
+             keyboard = types.InlineKeyboardMarkup()  # клавиатура принятия заказа
+             key_yes = types.InlineKeyboardButton(text='Принять заказ', callback_data='order')
+             keyboard.add(key_yes)
+
+             trip = ('Откуда: ' + row[0] + '\nКуда: ' + row[1] + '\nЦена: ' + str(row[2]) + '\nРастояние: ' + str(
+                 result)
+                     + ' КМ')
+             bot.send_message(message.chat.id, trip, reply_markup=keyboard)
+
+             if (i + 1) % 5 == 0:
+                 markup_trip = types.ReplyKeyboardMarkup(resize_keyboard=True,
+                                                         row_width=1)  ###Продолжение поиска поездки
+                 btn_search = types.KeyboardButton('Продолжить поиск')
+                 markup_trip.add(btn_search)
+                 bot.send_message(message.chat.id, "Для просмотра следущих заказов, нажмите \"Продолжить поиск\"",
+                                  reply_markup=markup_trip)
+                 break
+
+             i = i + 1
+
+     if message.text == "Продолжить поиск":
+         location_drive(message, i)
 
 
 
