cff2857b5c321548d2c11a4030e4b45fe9868ec0
ArtemFrun <49340615+ArtemFrun@users.noreply.github.com>
2019-04-29 12:54:40 +0300

    Update Trip.py

--- Trip.py
+++ Trip.py
@@ -18,7 +18,6 @@ def trip_pas(dispatch, expectation, price, lon_pas, lat_pas, chat_id, date_Creat
     day = datetime(now.year, now.month, now.day)
     rez = (now - day)
     now_sec = int(rez.total_seconds())
-    date_Create = time.strftime("%Y.%m.%d", time.localtime())
     info_trip = [dispatch, expectation, price, lon_pas, lat_pas, chat_id, status_trip, date_Create, now_sec]
     with sqlite3.connect("mydb.sqlite") as con:
         cur = con.cursor()
@@ -37,7 +36,7 @@ def cancel_trip_pas(chat_id):
 
 
 ###Отображение активных заказов
-def active_search_passenger(message, lon_dr,  lat_dr):
+def active_search_passenger(message, lon_dr,  lat_dr, i):
     now = datetime.now()
     day = datetime(now.year, now.month, now.day)
     rez = (now - day)
@@ -47,8 +46,7 @@ def active_search_passenger(message, lon_dr,  lat_dr):
     con = sqlite3.connect("mydb.sqlite")
     cur = con.cursor()
     with con:
-        cur.execute('SELECT * FROM trip WHERE data_seconds>? AND date_Create=? AND status_trip=1', (time_Create_dr,data_Create_dr))
-        i=0
+        cur.execute('SELECT * FROM trip WHERE data_seconds<? AND date_Create=? AND status_trip=1', (time_Create_dr, data_Create_dr))
         while True:
             row = cur.fetchone()
 
@@ -60,19 +58,26 @@ def active_search_passenger(message, lon_dr,  lat_dr):
                                  reply_markup=markup_menu)
                 return
 
-            result = float('{:.2f}'.format(geodesic((row[3],row[4]), (lon_dr,  lat_dr)).km))
+            result = float('{:.2f}'.format(geodesic((row[3], row[4]), (lon_dr,  lat_dr)).km))
 
-            trip = ('Откуда: ' + row[0] + '\nКуда: ' + row[1] + '\nЦена: ' + str(row[2]) + '\nРастояние: ' + str(result) + ' КМ')
-            bot.send_message(message.chat.id, trip)
+            keyboard = types.InlineKeyboardMarkup()  # клавиатура принятия заказа
+            key_yes = types.InlineKeyboardButton(text='Принять заказ', callback_data='Accept')
+            keyboard.add(key_yes)
+
+            trip = ('Откуда: ' + row[0] + '\nКуда: ' + row[1] + '\nЦена: ' + str(row[2]) + '\nРастояние: ' + str(result)
+                    + ' КМ')
+            bot.send_message(message.chat.id, trip, reply_markup=keyboard)
 
 
-            if i+1%5 == 0:
-                markup_trip = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)  ###переход к поиску поездки
+            if (i+1)%5 == 0:
+                markup_trip = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)  ###Продолжение поиска поездки
                 btn_search = types.KeyboardButton('Продолжить поиск')
                 markup_trip.add(btn_search)
                 bot.send_message(message.chat.id, "Для просмотра следущих заказов, нажмите \"Продолжить поиск\"",
                                  reply_markup=markup_trip)
-                if message.text == "Продолжить поиск":
-                    break
+                break
 
             i = i + 1
+
+    if message.text == "Продолжить поиск":
+        active_search_passenger(message, lon_dr,  lat_dr, i)
