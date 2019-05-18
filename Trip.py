99b5b3d213fb05e23f23da47934506fb39cb47a8
ArtemFrun <tjomka777@gmail.com>
2019-05-18 14:08:47 +0300

    Update Trip.py

--- Trip.py
+++ Trip.py
@@ -36,8 +36,32 @@ def change_trip_pas(chat_id, status_trip):
     return
 
 
+def START_SEARCH_PAS(message):
+    markup_menu = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)  ###переход к поиску поездки
+    btn_start = types.KeyboardButton('Поиск пассажира')
+    markup_menu.add(btn_start)
+    bot.send_message(message.chat.id, "Для начала поиска нажмите \"Поиск пассажира\"", reply_markup=markup_menu)
+    return
+
 ###Отображение активных заказов
-def active_search_passenger(message, lon_dr,  lat_dr, i):
+def active_search_passenger(message, num, count, lon_dr, lat_dr):
+    global keyboards
+    global trip_num
+    global order1
+    global order2
+    global order3
+    global order4
+    global order5
+    global counter
+    order1 = 0
+    order2 = 0
+    order3 = 0
+    order4 = 0
+    order5 = 0
+    counter = count
+    trip_num = num
+    keyboards = []
+    bot.send_message(message.chat.id, "Начало поиска")
     now = datetime.now()
     day = datetime(now.year, now.month, now.day)
     rez = (now - day)
@@ -47,44 +71,75 @@ def active_search_passenger(message, lon_dr,  lat_dr, i):
     con = sqlite3.connect("mydb.sqlite")
     cur = con.cursor()
     with con:
-        cur.execute('SELECT * FROM trip WHERE data_seconds<? AND date_Create=? AND status_trip=1', (time_Create_dr, data_Create_dr))
+        cur.execute('SELECT * FROM trip WHERE data_seconds<? AND date_Create=? AND status_trip=1',
+                    (time_Create_dr, data_Create_dr))
+        rows = cur.fetchall()
         while True:
-            row = cur.fetchone()
 
-            if row == None:
+            try:
+                row = rows[trip_num]
+            except IndexError:
                 markup_menu = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)  ###переход к поиску поездки
-                btn_search = types.KeyboardButton('Поиск пассажира')
-                markup_menu.add(btn_search)
+                btn_restart = types.KeyboardButton('Поиск пассажира')
+                markup_menu.add(btn_restart)
                 bot.send_message(message.chat.id, "Больше нету заказов. Для начала поиска нажмите \"Поиск пассажира\"",
                                  reply_markup=markup_menu)
-                return
+                trip_num = 0
+                return order1, order2, order3, order4, order5
 
-            result = float('{:.2f}'.format(geodesic((row[3], row[4]), (lon_dr,  lat_dr)).km))
+            result = float('{:.2f}'.format(geodesic((row[3], row[4]), (lon_dr, lat_dr)).km))
 
-            keyboard = types.InlineKeyboardMarkup()  # клавиатура принятия заказа
-            key_yes = types.InlineKeyboardButton(text='Принять заказ', callback_data='Accept')
-            keyboard.add(key_yes)
+            if trip_num == 0 + counter:
+                order1 = trip_num
+                keyboard = types.InlineKeyboardMarkup()  # клавиатура принятия заказа
+                key_yes = types.InlineKeyboardButton(text='Принять заказ', callback_data='trip1')
+                keyboard.add(key_yes)
+            elif trip_num == 1 + counter:
+                order2 = trip_num
+                keyboard = types.InlineKeyboardMarkup()  # клавиатура принятия заказа
+                key_yes = types.InlineKeyboardButton(text='Принять заказ', callback_data='trip2')
+                keyboard.add(key_yes)
+            elif trip_num == 2 + counter:
+                order3 = trip_num
+                keyboard = types.InlineKeyboardMarkup()  # клавиатура принятия заказа
+                key_yes = types.InlineKeyboardButton(text='Принять заказ', callback_data='trip3')
+                keyboard.add(key_yes)
+            elif trip_num == 3 + counter:
+                order4 = trip_num
+                keyboard = types.InlineKeyboardMarkup()  # клавиатура принятия заказа
+                key_yes = types.InlineKeyboardButton(text='Принять заказ', callback_data='trip4')
+                keyboard.add(key_yes)
+            elif trip_num == 4 + counter:
+                order5 = trip_num
+                keyboard = types.InlineKeyboardMarkup()  # клавиатура принятия заказа
+                key_yes = types.InlineKeyboardButton(text='Принять заказ', callback_data='trip5')
+                keyboard.add(key_yes)
 
-            trip = ('Откуда: ' + row[0] + '\nКуда: ' + row[1] + '\nЦена: ' + str(row[2]) + '\nРастояние: ' + str(result)
+            trip = ('Откуда: ' + row[0] + '\nКуда: ' + row[1] + '\nЦена: ' + str(row[2]) + '\nРастояние: ' + str(
+                result)
                     + ' КМ')
             bot.send_message(message.chat.id, trip, reply_markup=keyboard)
 
-
-            if (i+1)%5 == 0:
-                markup_trip = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)  ###Продолжение поиска поездки
+            if (trip_num + 1) % 5 == 0:
+                markup_trip = types.ReplyKeyboardMarkup(resize_keyboard=True,
+                                                        row_width=1)  ###Продолжение поиска поездки
                 btn_search = types.KeyboardButton('Продолжить поиск')
                 markup_trip.add(btn_search)
                 bot.send_message(message.chat.id, "Для просмотра следущих заказов, нажмите \"Продолжить поиск\"",
                                  reply_markup=markup_trip)
-                break
+                return order1, order2, order3, order4, order5
 
-            i = i + 1
+            trip_num = trip_num + 1
 
-    if message.text == "Продолжить поиск":
-        active_search_passenger(message, lon_dr,  lat_dr, i)
+###продолжить поиск
+def continue_search(message):
+    count = counter + 5
+    num = trip_num + 1
+    return count, num
 
 
 
+###Сообщение пассажиру о прийоме его заказа
 def message_for_passenger(message, time_arrival, chat_id_passenger, chat_id_drive):
     con = sqlite3.connect("mydb.sqlite")
     cur = con.cursor()
@@ -99,7 +154,7 @@ def message_for_passenger(message, time_arrival, chat_id_passenger, chat_id_driv
             if (row[8] == 0):
                 star = 0
             else:
-                star = int(row[9]) / int(row[8])
+                star = "{0:.2f}".format(int(row[9]) / int(row[8]))
 
             info_dr = ('Ваш заказ принят, машина будет через ' + str(time_arrival) + ' мин.\n\n' + 'Автомобиль марки: ' + row[7]
                        + '\nНомерной знак: ' + row[6] + '\nЦвет: ' + row[5] + '\n\nРейтинг водителя: ' + str(star)+ ' ⭐️')
@@ -107,6 +162,8 @@ def message_for_passenger(message, time_arrival, chat_id_passenger, chat_id_driv
             return
 
 
+
+###Сообщение о прибытие автомобиля
 def car_in_place(message, chat_id_passenger, chat_id_drive):
     con = sqlite3.connect("mydb.sqlite")
     cur = con.cursor()
@@ -119,7 +176,7 @@ def car_in_place(message, chat_id_passenger, chat_id_drive):
             if (row[8] == 0):
                 star = 0
             else:
-                star = int(row[9]) / int(row[8])
+                star = "{0:.2f}".format(int(row[9]) / int(row[8]))
 
             info_dr = ('Автомобиль на месте. Можите выходить.\n\n' + 'Автомобиль марки: ' + row[7]
                        + '\nНомерной знак: ' + row[6] + '\nЦвет: ' + row[5] + '\n\nРейтинг водителя: ' + str(star) + ' ⭐️')
@@ -128,6 +185,8 @@ def car_in_place(message, chat_id_passenger, chat_id_drive):
             return
 
 
+
+###Оценка за поездку
 def review_drive(message, chat_id_passenger):
     keyboard = types.InlineKeyboardMarkup()
     key_star = types.InlineKeyboardButton(text='5 ⭐️', callback_data='5_star')
@@ -143,6 +202,8 @@ def review_drive(message, chat_id_passenger):
     bot.send_message(chat_id_passenger, 'Поставте оценку за поездку.', reply_markup=keyboard)
     return
 
+
+###Запись оценки в рейтинг водителя
 def review_rating_drive(message, star, chat_id_drive):
     con = sqlite3.connect("mydb.sqlite")
     cur = con.cursor()
@@ -157,3 +218,15 @@ def review_rating_drive(message, star, chat_id_drive):
         cur.execute('UPDATE drive SET number_of_ratings=?, sum_of_ratings = ? WHERE chat_id=?', (num_reting, reting_dr, chat_id_drive))
 
         return
+
+
+
+### Начать поездку (переход на создания поездки)
+def START_TRIP(message):
+    markup_menu = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)  ###переход к поиску поездки
+    btn_location = types.KeyboardButton('Начать поездку')
+    markup_menu.add(btn_location)
+    bot.send_message(message.chat.id, "Для поиска автомобиля нажмите на кнопкую. Начать поездку",
+                         reply_markup=markup_menu)
+    return
+
